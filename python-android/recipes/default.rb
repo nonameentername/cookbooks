9d753933d8b9047899194b5ef0b7be7103ea8a02
werner mendizabal <nonameentername@gmail.com>
2011-12-18 20:48:06 -0600

    add build python-android to cookbook

--- python-android/recipes/default.rb
+++ python-android/recipes/default.rb
@@ -10,6 +10,7 @@ script "setup-python-android-repo" do
   user "vagrant"
   cwd "/home/vagrant/source"
   code "git clone git://github.com/nonameentername/python-android.git"
+  not_if "find . -name python-android"
 end
 
 script "bootstrap-python-android" do
@@ -17,4 +18,13 @@ script "bootstrap-python-android" do
   user "vagrant"
   cwd "/home/vagrant/source/python-android"
   code "./bootstrap.sh"
+  not_if "find . -name prebuilt"
+end
+
+bash "build-python-android" do
+  user "vagrant"
+  cwd "/home/vagrant/source/python-android"
+  code "./build.sh"
+  environment "HOME" => "/home/vagrant"
+  not_if "find . -name build"
 end
